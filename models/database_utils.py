e3fb788a153d55ec257ae1f9802ed6fdb1396fac
MTN-spec <mhandutakunda@gmail.com>
2025-04-18 03:01:30 +0200

    Create database_utils.py
    this is an SQL database format

---
+++ models/database_utils.py
@@ -0,0 +1,31 @@
+id	species	date	location	longitude	latitude	temperature_c	rainfall_mm	humidity_percent	wind_speed_m_s	altitude_m	notes	migration_likelihood
+1	Quelea quelea	2023-01-01	Matendere	-32.30072	-20.72033	47	0.3	55.7	8.9	1087	Observed during morning hours.	0
+2	Quelea quelea	2023-10-27	Mukwazi	-32.00989	-20.84601	46.9	10.8	49.7	9.8	1146	Low activity due to high temperatures.	0
+3	Quelea quelea	2023-12-15	Masapas	-31.86565	-20.58256	41.2	18.8	37	6.5	660	Observed during morning hours.	0
+4	Quelea quelea	2023-02-26	MSaize	-32.06392	-20.24692	46.5	14.6	55.1	8	503	High activity near water bodies.	0
+5	Quelea quelea	2023-02-11	MSaize	-32.17619	-20.13646	43.5	2	39.9	2.3	1193	High activity near water bodies.	0
+6	Quelea quelea	2023-08-06	Mukwazi	-32.1826	-20.68845	41.3	0.8	48.3	5.7	1142	Low activity due to high temperatures.	0
+7	Quelea quelea	2023-01-05	Savuli	-32.01596	-19.96054	35.8	1.8	42.9	7.9	698	High activity near water bodies.	0
+8	Quelea quelea	2023-02-28	Hammond	-32.02076	-20.21528	40.8	10.7	30.5	2.6	890	Observed during morning hours.	0
+9	Quelea quelea	2023-08-28	Mukwasine/Impala	-32.31747	-20.48972	38.6	19.9	39.8	3.3	1398	High activity near water bodies.	0
+10	Quelea quelea	2023-10-26	MSaize	-32.18902	-20.34061	44.5	5.8	49.6	8.4	624	Observed during morning hours.	0
+11	Quelea quelea	2023-10-14	Matendere	-32.32093	-20.23313	41	10.4	50.2	5.3	1345	Observed during morning hours.	0
+12	Quelea quelea	2023-10-30	Matendere	-32.30584	-20.61112	39.9	4.1	50.6	5.4	766	High activity near water bodies.	0
+13	Quelea quelea	2023-09-28	Humani	-31.82141	-20.64488	40	19.9	56.9	7.3	1209	Observed during morning hours.	0
+14	Quelea quelea	2023-09-03	Matendere	-31.9368	-20.2724	43.8	3	31.2	4.6	1295	Birds were flying in a V-formation.	0
+15	Quelea quelea	2023-10-30	Mukwasine/Impala	-31.80142	-20.79562	35.7	3.7	42.1	6.8	820	Observed during morning hours.	0
+16	Quelea quelea	2023-10-27	Umkondo Mine	-31.79813	-20.01152	37.9	1.1	36.2	9.1	1477	Birds were flying in a V-formation.	0
+17	Quelea quelea	2023-04-14	MSaize	-32.18951	-20.35435	38	6.3	37	6.5	851	Observed during morning hours.	0
+18	Quelea quelea	2023-08-06	Chapunfu	-31.89865	-20.55239	43.8	16.1	41.5	8.7	556	High activity near water bodies.	0
+19	Quelea quelea	2023-11-18	Mukwazi	-32.32396	-20.19464	36.3	12.3	58.8	2.7	802	Birds were flying in a V-formation.	0
+20	Quelea quelea	2023-05-20	Mukwasine/Impala	-32.01291	-20.17422	45.1	17.9	46.6	2.9	1411	Low activity due to high temperatures.	0
+21	Quelea quelea	2023-04-21	Arda	-31.85415	-20.11827	37.7	10.8	55.1	7.9	1214	High activity near water bodies.	0
+22	Quelea quelea	2023-04-22	Senuko	-32.17661	-20.51732	44.4	8.4	36.1	5.5	1467	Birds were flying in a V-formation.	0
+23	Quelea quelea	2023-12-03	Arda	-31.96688	-20.10805	35.4	7.9	54.7	9.6	1392	High activity near water bodies.	0
+24	Quelea quelea	2023-01-31	Hammond	-31.95439	-20.80603	45.8	16.2	46.7	5.4	1393	Low activity due to high temperatures.	0
+25	Quelea quelea	2023-10-19	Savuli	-32.10958	-20.36595	45.9	8.9	35.9	8.9	1416	Low activity due to high temperatures.	0
+26	Quelea quelea	2023-11-10	Levanga	-32.3083	-20.13845	35.9	8.3	32.7	8.9	1366	Birds were flying in a V-formation.	0
+27	Quelea quelea	2023-10-20	Umkondo Mine	-32.25014	-20.86592	36	7	56.1	6.9	1199	Low activity due to high temperatures.	0
+28	Quelea quelea	2023-07-05	MSaize	-31.96257	-20.36635	46.5	17.7	45	9.1	1385	Birds were flying in a V-formation.	0
+29	Quelea quelea	2023-10-17	Angus	-32.04125	-20.10964	41.8	11.7	48.3	2.8	1314	Low activity due to high temperatures.	0
+30	Quelea quelea	2023-07-12	Matendere	-31.82346	-19.95544	38.1	4.2	45.5	6.6	1084	High activity near water bodies.	0
